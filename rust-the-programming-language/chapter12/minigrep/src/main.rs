d5892d8e1a1f679f58655491221d8c7a77385f38
dynamicmemory <joshua.hahn@live.com>
2025-06-13 14:56:22 +1000

    rust is still a little... touched

--- rust-the-programming-language/chapter12/minigrep/src/main.rs
+++ rust-the-programming-language/chapter12/minigrep/src/main.rs
@@ -1,28 +1,21 @@
-use std::fs;
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
-
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     // let (query, file_path) = parse_config(&args);
-    // println!("Searching for {}", query);
-    // println!("In file {}", file_path);
-
-    let file_content = fs::read_to_string(config.file_path).expect("File does not exist");
-
-    println!("{file_content}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config{query, file_path}
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    
+    if let Err(e) = minigrep::run(config) {
+        println!("application error: {e}");
+        process::exit(1);
+    }
 }
