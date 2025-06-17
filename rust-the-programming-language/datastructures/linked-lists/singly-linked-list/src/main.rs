6bc3a58af609751c499996974c47e901941431ce
dynamicmemory <joshua.hahn@live.com>
2025-06-17 14:38:50 +1000

    single linked list done

--- rust-the-programming-language/datastructures/linked-lists/singly-linked-list/src/main.rs
+++ rust-the-programming-language/datastructures/linked-lists/singly-linked-list/src/main.rs
@@ -1,4 +1,6 @@
-use std::ptr;
+use std::alloc::dealloc;
+use std::ptr::{self, null, null_mut};
+use std::fmt::{Display, Pointer};
 
 struct Node<T> {
     next: *mut Node<T>, 
@@ -9,39 +11,87 @@ struct List<T> {
     head: *mut Node<T>,
 }
 
-impl<T> List<T> {
-    fn init(key: T) -> Self {
-        let inital_node = Box::new(Node {
-            next: ptr::null_mut(),
-            key,
+impl<T: Display + PartialEq> List<T> {
+    fn new() -> Self {
+        List { head: ptr::null_mut() }
+    }
+
+    fn empty(&self) -> bool {
+        self.head.is_null() 
+    }
+
+    fn add(&mut self, key: T) {
+        let node = Box::new(Node {
+            next: self.head, 
+            key: key,
         });
-        
-        let head = Box::into_raw(inital_node);
 
-        List { head }
+        self.head = Box::into_raw(node);
     }
 
-    fn empty() {
+    fn search(&self, key: T) -> *mut Node<T> {
+        unsafe {
+            let mut current = self.head;
+            while !current.is_null() {
+                if (*current).key == key {
+                    return current
+                }
+                current = (*current).next;
+            } 
+            return null_mut()
+        }
     }
 
-    fn add() {
-    }
+    fn delete(&mut self, key: T) {
+        unsafe {
+            let current = self.search(key);
+            if current == null_mut() {
+                return 
+            }
 
-    fn search() {
-    }
+            if current == self.head {
+                self.head = (*current).next;
+            }
+            else {
+                let mut prev = self.head;
 
-    fn delete() {
+                if prev.is_null() {
+                    return 
+                }
+                while (*prev).next != current {
+                    prev = (*prev).next;
+                }
+                (*prev).next = (*current).next;
+            }
+            drop(Box::from_raw(current));
+        }
     }
    
-    fn print() {
+
+    fn print(&self) {
+        unsafe {
+            if !self.empty() {
+                let mut current = self.head;
+                while !current.is_null() {
+                    println!("{}", (*current).key);
+                    current = (*current).next;
+                }
+            }
+        }
     }
 }
 
 
 fn main() {
-    let list = List::init(66);
-
-    unsafe {
-        println!("{:?}", (*list.head).key);
-    }
+    let mut list: List<i32> = List::new();
+    list.add(69);
+    list.add(420);
+    list.print();
+ 
+    list.delete(419);
+    list.add(666);
+    list.print();
+    list.delete(69);
+    list.print();
+    
 }
