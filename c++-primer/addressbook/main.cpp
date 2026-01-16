23edfbb45dd0404c2f7caf05b2ae5b34d895c0d8
dynamicmemory <joshua.hahn@live.com>
2026-01-16 15:23:25 +1000

    Address book has add, delte, exit and persistance

--- c++-primer/addressbook/main.cpp
+++ c++-primer/addressbook/main.cpp
@@ -3,9 +3,11 @@
 // Sort contacts 
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
 #include <cstdint>
-using std::string, std::vector;
+using std::string, std::vector, std::cout, std::cin, std::endl;
 
 class Contact {
 public:
@@ -30,37 +32,117 @@ class AddressBook {
 public:
     AddressBook(string file) : filename(file) {}
 
-    void add_contact(Contact c);
-    void edit_contact(Contact c);
-    void delete_contact(Contact c);
+    void add_contact(string a, string b, string c);
+    void edit_contact(string a, string b, string c);
+    void delete_contact(string a);
+    void update_addressbook();
+    vector<Contact> get_contact_list();
+    vector<Contact> populate_contacts();
 
 private:
     string filename;
-    vector<Contact> contact_list;
-    vector<Contact> populate_contacts();
+    vector<Contact> contact_list = populate_contacts();
 };
 
-vector<Contact> populate_contacts() {
+vector<Contact> AddressBook::populate_contacts() {
     vector<Contact> temp;
-    // Try to read file and parse in contents 
+    // Open a filestream for the addressbook filename
+    std::ifstream fin(filename);
+    // If file doesnt exist, create it
+    if (!fin) {
+        std::ofstream create(filename);
+        fin.open(filename);
+    }
+    
+    string line;
+    while (std::getline(fin, line)) {
+        std::stringstream ss(line);
+        string fname, lname, number;
+
+        if (std::getline(ss, fname, ',') &&
+                std::getline(ss, lname, ',') &&
+                std::getline(ss, number, ',')) {
+            Contact new_contact(fname, lname, number);
+            temp.push_back(new_contact);
+        }
+    }
+
     return temp;
 }
 
-void add_contact(Contact c) {
-
+/* Returns a copy of the class variable 'contact_list' */
+vector<Contact> AddressBook::get_contact_list() {
+    vector<Contact> copy = contact_list;
+    return copy;
 }
 
-void edit_contact(Contact c) {
-
+void AddressBook::add_contact(string fname, string lname, string number) {
+    Contact c(fname, lname, number);
+    contact_list.push_back(c);
+    update_addressbook();
 }
 
-void delete_contact(Contact c) {
+void AddressBook::edit_contact(string fname, string lname, string number) {
+    // Edits a match entry to users new fields 
+}
 
+void AddressBook::delete_contact(string number) {
+    for (auto it=contact_list.begin(); it < contact_list.end(); ++it) {
+        if (it->get_number() == number) { 
+            contact_list.erase(it);
+            cout << number << " was removed from your contacts" << endl;
+            update_addressbook(); 
+            return;
+        }
+    }
+    cout << number << " is not in your contacts." << endl;
+}
+
+void AddressBook::update_addressbook() {
+    std::ofstream fout(filename);
+    for (auto contact : contact_list)
+        fout << contact.get_firstname() << ','
+            << contact.get_lastname() << ','
+            << contact.get_number() << '\n';
 }
 
 int main(int argc, char *argv[]) {
-    string fname = "./contacts";
+    string fname = "./contacts.txt";
     AddressBook book(fname);
+
+    cout << "First Name\tLast Name\tPhone Number" << "\n";
+    while (true) {
+        vector<Contact> list = book.get_contact_list(); 
+        for (auto contact : list) {
+            cout << contact.get_firstname() << "\t\t" <<
+                contact.get_lastname() << "\t\t" <<
+                contact.get_number() << endl;
+        }
+
+        string command;
+        cout << "Enter a command (add, edit, delete, exit)\n>> ";
+        cin >> command;
+
+        if (command == "add") {
+            string fname, lname, number;
+            cout << "Enter the contacts first name.\n>> ";
+            cin >> fname;
+            cout << "Enter the contacts last name.\n>> ";
+            cin >> lname;
+            cout << "Enter the contacts number.\n>> ";
+            cin >> number;
+            book.add_contact(fname, lname, number);
+        }
+        else if (command == "delete") {
+            string number;
+            cout << "Enter the contacts number.\n>> ";
+            cin >> number;
+            book.delete_contact(number);
+        }
+        else if (command == "exit") {
+            return 0;
+        }
+    }
 
     return 0;
 }
