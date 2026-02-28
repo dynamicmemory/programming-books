135904bb4ead961143b36dbba2975893a1ca39e7
dynamicmemory <joshua.hahn@live.com>
2026-02-28 19:03:09 +1000

    a touch of C a day keeps the rust away

---
+++ the-c-programming-language/chp1/syntax.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+    int parens, curls, quotes, n;
+    parens = curls = quotes = 0;
+    char c;
+    n = 1;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') ++n;
+        if (c == '(') ++parens;
+        if (c == ')' && parens == 0) {
+            printf(" <<parenthesis error>> Line %3d", n);
+            --parens;
+        }
+        if (c == '{') ++curls;
+        if (c == '}') {
+            printf(" <<curly brace error>> Line %3d", n);
+            --curls;
+        }
+        if (c == '"') {
+            printf(" <<quotation error>> Line %3d", n);
+            ++quotes;
+        }
+        putchar(c);
+    }
+
+    return 0;
+}
